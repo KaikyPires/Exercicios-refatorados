65fefd5a5dcb8b5b03f8eae17af44879e4388b95
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-19 14:17:49 -0300

    Add Personagem class with attack strategy implementation

---
+++ Personagem.java
@@ -0,0 +1,15 @@
+public class Personagem{
+    private String nome;
+    private IAtaque estrategiaDeAtaque;
+
+    public Personagem(String nome, IAtaque estrategiaDeAtaque){
+        this.nome = nome;
+        this.estrategiaDeAtaque = estrategiaDeAtaque;
+    }
+
+    public void ataque(){
+        System.out.println(nome + "");
+        estrategiaDeAtaque.ataque();
+    }
+
+}
